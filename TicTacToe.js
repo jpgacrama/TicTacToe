ca216084071c9f6192a3983b20208aedee0d0820
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2020-12-02 14:34:18 +0800

    Changed how the renderSquare() function is laid out
    There are no other functional changes besides this

--- TicTacToe.js
+++ TicTacToe.js
@@ -4,13 +4,14 @@ function Square(props) {
       {props.value}
     </button>
   );
+}
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext : true,
+      xIsNext: true,
     };
   }
 
@@ -39,13 +40,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
